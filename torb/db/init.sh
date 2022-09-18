e0656c279b924c2748b1c8608895345ac44d7e9c
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-09-18 23:13:52 +0900

    Add initialize

--- torb/db/init.sh
+++ torb/db/init.sh
@@ -6,15 +6,18 @@ BENCH_DIR="$ROOT_DIR/bench"
 
 export MYSQL_PWD=isucon
 
-mysql -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
-mysql -uisucon torb < "$DB_DIR/schema.sql"
+#mysql -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
+#mysql -uisucon torb < "$DB_DIR/schema.sql"
 
-if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
-  echo "Run the following command beforehand." 1>&2
-  echo "$ ( cd \"$BENCH_DIR\" && bin/gen-initial-dataset )" 1>&2
-  exit 1
-fi
+#if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
+#  echo "Run the following command beforehand." 1>&2
+#  echo "$ ( cd \"$BENCH_DIR\" && bin/gen-initial-dataset )" 1>&2
+#  exit 1
+#fi
 
-mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
-gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
-mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+#mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
+#gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
+#mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+
+
+mysql -uisucon -pisucon torb < "$DB_DIR/initial-datase-split.sql"
