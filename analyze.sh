825e4d4f40afd8e5e9164805c46e134322029845
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-09-18 20:10:36 +0900

    update alpm

--- analyze.sh
+++ analyze.sh
@@ -15,7 +15,7 @@ mkdir -p ${result_dir}
 #sudo journalctl --since="${prepared_time}" | gzip -9c > "${data_dir}/journal.log.gz"
 
 # alp
-ALPM="/api/admin/tenants/add,/api/admin/tenants/billing,/api/organizer/players,/api/organizer/players/add,/api/organizer/player/[a-zA-Z0-9]+/disqualified,/api/organizer/competitions/add,/api/organizer/competition/[a-zA-Z0-9]+/finish,/api/organizer/competition/[a-zA-Z0-9]+/score,/api/organizer/billing,/api/organizer/competitions,/api/player/player/[a-zA-Z0-9]+,/api/player/competition/[a-zA-Z0-9]+/ranking,/api/player/competitions,/api/me,/initialize"
+ALPM="/,/initialize,/api/users,/api/users/\d,/api/actions/login,/api/actions/logout,/api/events,/api/events/\d,/api/events/\d/actions/reserve,/api/events/\d/sheets/[A-Za-z]+/\d/reservation,/admin/,/admin/api/actions/login,/admin/api/actions/logout,/admin/api/events,/admin/api/events,/admin/api/events/\d,/admin/api/events/\d/actions/edit,/admin/api/reports/events/\d/sales,/admin/api/reports/sales"
 OUTFORMT=count,1xx,2xx,3xx,4xx,5xx,method,uri,min,max,sum,avg,p95,min_body,max_body,avg_body
 alp ltsv --file=${nginx_access_log} \
   --nosave-pos \
